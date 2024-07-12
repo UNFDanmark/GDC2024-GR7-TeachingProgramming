446b79c517c86c6af57aae01122cf9ea812d4d4a
Deltager <teknisk+GDC2024-GR7@game.unf.dk>
2024-07-12 16:42:10 +0200

    Start på produktion

--- Opgaver/Assets/enemyEvil.cs
+++ Opgaver/Assets/enemyEvil.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyEvil : MonoBehaviour
 {
+    public int speed = 10;
+    public Transform rotation;
+    public Transform CamR;
+    public Rigidbody Tank;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hey, jeg er ond");
+        Tank = GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        float rotation = Input.GetAxisRaw("Rotation Horizontal");
+
+        Vector3 movement = new Vector3();
+        movement.x = horizontal * speed;
+        movement.z = vertical * speed;
+        Tank.velocity = movement;
+        transform.Rotate(0,rotation,0);
         
-       
-
-
+    }
 }
